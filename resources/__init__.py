b36f092f0d97c8f3e5019007ef52aa81202b1dbd
Nicolás Miñán <nm.imagenysonido@gmail.com>
2022-02-25 16:31:54 -0300

    clean reaosurces/__init__.py

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,7 +1,5 @@
 from abc import ABC
-import requests
 
-from ..response import Response
 from ..connection import ConnectionSetup
 from ..constants import API_BASE_PATH
 
@@ -16,15 +14,3 @@ class Resource(ABC):
     def url(self): return f"{API_BASE_PATH}{self._resource_path}"
     @property
     def headers(self) -> dict: return self._connection_setup.headers
-
-    # Actions -----------------------------------------------------------------
-    # def _get(self, page=None) -> Response:
-    #     params = dict()
-
-    #     if page is not None:
-    #         params["page"] = page
-
-    #     return Response(requests.get(self.url, headers=self._connection_setup.headers, params=params))
-
-    # def _post(self, data: dict) -> Response:
-    #     return Response(requests.post(self.url, data=data, headers=self._connection_setup.headers))
